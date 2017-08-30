fc5c47ce96734c57987a928b750522b9c6a48c15
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-08-30 18:00:01 +0300

    added sci.php which gets the data from the contact form, proccesses it, saves it in contactdb.json and displays a message

---
+++ Online_Shop/sci.php
@@ -0,0 +1,34 @@
+<?php
+    require_once "header.php";
+    echo "<nav>
+    <div id=\"nav\" class=\"float-left\">
+        <fieldset id=\"nav_fieldset\">
+            <legend>Menu</legend>
+            <ul>
+                <li><a href=\"index.php\">Home</a></li>
+                <li><a href=\"index.php?page=about_us\">About Us</a></li>
+                <li><a href=\"index.php?page=contacts\">Contacts</a></li>
+                <li><a href=\"index.php?page=catalog\">Catalog</a></li>
+                <li><a href=\"index.php?page=blog\">Blog</a></li>
+            </ul>
+        </fieldset>
+    </div>
+</nav>";
+
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["number"]) && isset($_POST["message"]) && isset($_POST["send"])) {
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $number = $_POST["number"];
+    $message = $_POST["message"];
+
+    $contacts = json_decode(file_get_contents("contactdb.json"), true);
+    $contacts[] = array("name" => $name, "email" => $email, "number" => $number, "message" => $message);
+    file_put_contents("contactdb.json", json_encode($contacts));
+
+    echo "<br><main class=\"float-left\">
+            <div class=\"sub-container\">Thank you for contacting us $name. We will get back to you as soon as possible!</div>
+          </main>";
+
+}
+require_once "footer.php";
