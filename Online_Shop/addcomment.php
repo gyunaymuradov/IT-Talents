4c8f115f94029e5c18c5b3367aa13a1d28545972
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-04 11:35:35 +0300

    added functionality to comment are on news, now if you are logged name input in comment area does not appear. Instead when saving the comment the username is taken from the session.

--- Online_Shop/addcomment.php
+++ Online_Shop/addcomment.php
@@ -1,17 +1,32 @@
 <?php
+session_start();
+if ($_SESSION["logged"] == true) {
+    if (isset($_POST["comment_content"]) && isset($_POST["comment"])) {
+        $commentatorName = $_SESSION["username"];
+        $comment = $_POST["comment_content"];
+        date_default_timezone_set("Europe/Berlin");
+        $date = date('d-m-Y');
+        $newId = $_POST["current_id"];
+        $commentsFile = file_get_contents("comments.json");
+        $commentsArr = json_decode($commentsFile, true);
 
-if (isset($_POST["commentator"]) && isset($_POST["comment_content"]) && isset($_POST["comment"])) {
-    $commentatorName = $_POST["commentator"];
-    $comment = $_POST["comment_content"];
-    date_default_timezone_set("Europe/Berlin");
-    $date = date('d-m-Y');
-    $newId = $_POST["current_id"];
-    $commentsFile = file_get_contents("comments.json");
-    $commentsArr = json_decode($commentsFile, true);
+        $commentsArr[$newId][] = array("author" => $commentatorName, "comment" => $comment, "date" => $date);
+        file_put_contents("comments.json", json_encode($commentsArr));
+    }
+} else {
+    if (isset($_POST["commentator"]) && isset($_POST["comment_content"]) && isset($_POST["comment"])) {
+        $commentatorName = $_POST["commentator"];
+        $comment = $_POST["comment_content"];
+        date_default_timezone_set("Europe/Berlin");
+        $date = date('d-m-Y');
+        $newId = $_POST["current_id"];
+        $commentsFile = file_get_contents("comments.json");
+        $commentsArr = json_decode($commentsFile, true);
 
-    $commentsArr[$newId][] = array("author" => $commentatorName, "comment" => $comment, "date" => $date);
-    file_put_contents("comments.json", json_encode($commentsArr));
+        $commentsArr[$newId][] = array("author" => $commentatorName, "comment" => $comment, "date" => $date);
+        file_put_contents("comments.json", json_encode($commentsArr));
 
-    header("Location:index.php?page=news&id=$newId");
+    }
 }
+    header("Location:index.php?page=news&id=$newId");
 ?>
