8c87e89270bd0865c2814fce5ddc5218f86b42bd
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-17 14:36:15 +0300

    create page working now and adding new products to db and after successfull addition redirecting to show.php with the added item

--- e-shop/public/staff/create.php
+++ e-shop/public/staff/create.php
@@ -1,14 +1,39 @@
 <?php require_once '../../private/initialize.php'?>
-
-
 <?php require_once '../../private/shared/staff_header.php'; ?>
 
+<?php
+    if (isPostRequest()) {
+        $product = [];
+        $product['title'] = $_POST['title'] ?? '';
+        $product['price'] = $_POST['price'] ?? '';
+        $product['description'] = $_POST['description'] ?? '';
+        $imageTmpName = $_FILES['image']['tmp_name'];
+
+        if (is_uploaded_file($imageTmpName)) {
+            $imageRealName = $_FILES['image']['name'];
+
+            $dir = '../../private/images/';
+            $src = $dir . $imageRealName;
+
+            move_uploaded_file($imageTmpName, $src);
+
+            $product['image'] = $src;
+
+            $result = insertProduct($product);
+
+            if ($result == true) {
+                $newId = mysqli_insert_id($db);
+                redirectTo('/e-shop/public/staff/show.php?id=' . $newId);
+            }
+        }
+    }
+?>
 
 
-<div class="product new">
+<div>
     <h1>Add Product</h1>
 
-
+    <p><a class="back-link" href="products.php">&laquo; Back</a></p>
     <form action="create.php" method="post" enctype="multipart/form-data">
         <dl>
             <dt>Product Title</dt>
@@ -31,11 +56,11 @@
         <dl>
             <dt>Images</dt>
             <dd>
-                <input type="file" required>
+                <input type="file" name="image" required>
             </dd>
         </dl>
         <div id="operations">
-            <input type="submit" value="Create Subject" />
+            <input type="submit" value="Add product" />
         </div>
     </form>
 
