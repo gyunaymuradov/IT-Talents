730168209adb64d37c3ee11bc951a7a1a88d09bf
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-18 08:04:50 +0300

    added function to update existing record in the db

--- e-shop/private/db_query_functions.php
+++ e-shop/private/db_query_functions.php
@@ -42,3 +42,24 @@ function findProductById($id) {
     $product = mysqli_fetch_assoc($result);
     return $product;
 }
+
+function updateProduct($product) {
+    global $db;
+
+    $sql = "UPDATE products SET ";
+    $sql .= "title='" . dbEscape($db, $product['title']) . "', ";
+    $sql .= "price='" . dbEscape($db, $product['price']) . "', ";
+    $sql .= "description='" . dbEscape($db, $product['description']) . "', ";
+    $sql .= "image='" . dbEscape($db, $product['image']) . "' ";
+    $sql .= "WHERE id='" . dbEscape($db, $product['id']) . "' ";
+    $sql .= "LIMIT 1";
+
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        return true;
+    } else {
+        echo mysqli_error($db);
+        dbDisconnect($db);
+        exit;
+    }
+}
