7103240399311163a75670a69dc7aab0762ea329
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-23 18:56:57 +0300

    added validation when creating new admin and displaying error for invalid entered data

--- e-shop/public/staff/admins/create.php
+++ e-shop/public/staff/admins/create.php
@@ -3,7 +3,7 @@
 require_once '../../../private/initialize.php';
 
 requireLogin();
-
+$result = '';
 if (isPostRequest()) {
     $admin = [];
     $admin['firstName'] = $_POST['firstName'] ?? '';
@@ -11,11 +11,11 @@ if (isPostRequest()) {
     $admin['username'] = $_POST['username'] ?? '';
     $admin['email'] = $_POST['email'] ?? '';
     $admin['password'] = $_POST['password'] ?? '';
-//    $admin['confirmPassword'] = $_POST['confirmPassword'] ?? '';
+    $admin['confirmPassword'] = $_POST['confirmPassword'] ?? '';
 
     $result = insertAdmin($admin);
 
-    if ($result['affectedRows'] == 1) {
+    if (array_key_exists('affectedRows', $result)) {
         $newId = $result['lastInsertId'];
         redirectTo('/e-shop/public/staff/admins/show.php?id=' . $newId);
     }
@@ -29,42 +29,44 @@ require_once '../../../private/shared/staff_header.php';
 <div class="container">
     <h1>Add Admin</h1>
 
+    <?php displayErrors($result); ?>
+
     <p><a class="back-link" href="index.php">&laquo; Back</a></p>
     <form action="create.php" method="post">
         <dl>
             <dt>First name</dt>
             <dd>
-                <input type="text" name="firstName" value="" required>
+                <input type="text" name="firstName" value="" >
             </dd>
         </dl>
         <dl>
             <dt>Last name</dt>
             <dd>
-                <input type="text" name="lastName" value="" required>
+                <input type="text" name="lastName" value="" >
             </dd>
         </dl>
         <dl>
             <dt>Username</dt>
             <dd>
-                <input type="text" name="username" value="" required>
+                <input type="text" name="username" value="" >
             </dd>
         </dl>
         <dl>
             <dt>Email</dt>
             <dd>
-                <input type="text" name="email" value="" required>
+                <input type="text" name="email" value="" >
             </dd>
         </dl>
         <dl>
             <dt>Password</dt>
             <dd>
-                <input type="password" name="password" value="" required>
+                <input type="password" name="password" value="" >
             </dd>
         </dl>
         <dl>
             <dt>Confirm password</dt>
             <dd>
-                <input type="password" name="confirmPassword" value="" required>
+                <input type="password" name="confirmPassword" value="" >
             </dd>
         </dl>
         <small>
