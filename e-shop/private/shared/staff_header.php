6ddf6da9b09ff777372b7cd2499496d8cdfe5504
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-23 14:36:04 +0300

    added logout button and the current logged user display

--- e-shop/private/shared/staff_header.php
+++ e-shop/private/shared/staff_header.php
@@ -4,9 +4,18 @@
     <head>
         <title><?php echo $pageTitle; ?></title>
         <meta charset="utf-8">
-        <link rel="stylesheet" media="all" href="../../public/stylesheets/staff_stylesheet.css" type="text/css" />
+        <link rel="stylesheet" media="all" href="<?php echo getUrl('/stylesheets/staff_stylesheet.css'); ?>" type="text/css" />
     </head>
     <body>
         <header>
             <h2>E-SHOP Staff Area</h2>
         </header>
+
+<?php if (isset($_SESSION['adminId'])) { ?>
+
+    <div class="btn-container">
+        <span>Admin: <?php echo $_SESSION['username']; ?></span>&nbsp;&nbsp;&nbsp;
+        <span><a href="../../public/staff/logout.php">Logout</a></span>
+    </div>
+
+<?php } ?>
