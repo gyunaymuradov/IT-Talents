f45f0fa758cd1f14360769e290067cee5344a9b9
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-23 19:00:43 +0300

    added validation for creating new admin, also fixed some of the non-working functions

--- e-shop/private/db_query_functions.php
+++ e-shop/private/db_query_functions.php
@@ -1,4 +1,5 @@
 <?php
+
 function findAllProducts() {
     global $db;
 
@@ -32,8 +33,7 @@ function updateProduct($product) {
 
     $statement = $db->prepare("UPDATE products SET title = :title, price = :price, description = :description, image = :image WHERE id = :id LIMIT 1");
     $statement->execute($product);
-    $id = $db->lastInsertId();
-    $result = ['lastInsertId' => $id, 'affectedRows' => $statement];
+    $result = ['updatedId' => $product['id'], 'success' => true];
     return $result;
 }
 
@@ -62,23 +62,15 @@ function findAdminById($id) {
     return $admin;
 }
 
-function insertAdmin($admin) {
-    global $db;
-
-    $statement = $db->prepare("INSERT INTO admins (first_name, last_name, username, email, password) VALUES (:firstName, :lastName, :username, :email, :password)");
-    $statement->execute($admin);
-    $id = $db->lastInsertId();
-    $result = ['lastInsertId' => $id, 'affectedRows' => $statement];
-    return $result;
-}
-
 function updateAdmin($admin) {
     global $db;
 
-    $statement = $db->prepare("U{DATE admins SET first_name = :firstName, last_name = :lastName, username =:username, email = :email, password = :password");
+    $statement = $db->prepare("UPDATE admins SET first_name = :firstName, last_name = :lastName, username = :username, email = :email, password = :password WHERE id = :id LIMIT 1");
     $statement->execute($admin);
-    $id = $db->lastInsertId();
-    $result = ['lastInsertId' => $id, 'affectedRows' => $statement];
+    $result = ['updatedId' => $admin['id'], 'success' => true];
+    if ($_SESSION['adminId'] == $admin['id']) {
+    $_SESSION['username'] = $admin['username'];
+    }
     return $result;
 }
 
@@ -97,4 +89,110 @@ function findAdminByUsername($username) {
     $statement->execute(array("username" => $username));
     $admin = $statement->fetch(PDO::FETCH_ASSOC);
     return $admin;
+}
+
+function validateAdmin($admin) {
+
+    $firstNameIsBlank = false;
+    if(isBlank($admin['firstName'])) {
+        $firstNameIsBlank = true;
+        $errors[] = "First name cannot be blank.";
+    }
+
+    // check first name size
+    if (!$firstNameIsBlank) {
+        if (!hasLengthGreaterThan($admin['firstName'], 2)) {
+            $errors[] = "First name must be at least 2 characters long.";
+        } else if (!hasLengthLessThan($admin['firstName'], 12)) {
+            $errors[] = "First name cannot be longer than 12 characters.";
+        }
+    }
+
+    $lastNameIsBlank = false;
+    if(isBlank($admin['lastName'])) {
+        $lastNameIsBlank = true;
+        $errors[] = "Last name cannot be blank.";
+    }
+
+    // check last name size
+    if (!$lastNameIsBlank) {
+        if (!hasLengthGreaterThan($admin['lastName'], 2)) {
+            $errors[] = "Last name must be at least 2 characters long.";
+        } else if (!hasLengthLessThan($admin['lastName'], 12)) {
+            $errors[] = "Last name cannot be longer than 12 characters.";
+        }
+    }
+
+    $emailIsBlank = false;
+    if(isBlank($admin['email'])) {
+        $emailIsBlank = true;
+        $errors[] = "Email cannot be blank.";
+    }
+
+    // check email size
+    if (!$emailIsBlank) {
+        if (!hasLengthGreaterThan($admin['email'], 5)) {
+            $errors[] = "Email must contain 5 or more characters.";
+        } else if (!hasLengthLessThan($admin['email'], 25)) {
+            $errors[] = "Email cannot be longer than 25 characters.";
+        }
+    }
+
+    $usernameIsBlank = false;
+    if(isBlank($admin['username'])) {
+        $usernameIsBlank = true;
+        $errors[] = "Username cannot be blank.";
+    }
+
+    // check username size
+    if (!$usernameIsBlank) {
+        if (!hasLengthGreaterThan($admin['username'], 5)) {
+            $errors[] = "Username must contain 5 or more characters.";
+        } else if (!hasLengthLessThan($admin['username'], 12)) {
+            $errors[] = "Username cannot be longer than 12 characters.";
+        }
+
+        if (findAdminByUsername($admin['username'])) {
+            $errors[] = "Username already exist. Try another.";
+        }
+    }
+
+    $passwordIsBlank = false;
+    if(isBlank($admin['password'])) {
+        $passwordIsBlank = true;
+        $errors[] = "Password cannot be blank.";
+    }
+
+    // check password size
+    if (!$passwordIsBlank) {
+        if (!hasLengthGreaterThan($admin['password'], 6)) {
+            $errors[] = "Password must be at least 6 characters long.";
+        } else if (!hasLengthLessThan($admin['password'], 12)) {
+            $errors[] = "Password cannot be longer than 12 characters.";
+        }
+    }
+
+
+    if(isBlank($admin['confirmPassword'])) {
+        $errors[] = "Confirm password cannot be blank.";
+    } else if ($admin['password'] !== $admin['confirmPassword']) {
+        $errors[] = "Password and confirm password does not match.";
+    }
+
+    return $errors;
+}
+
+function insertAdmin($admin) {
+    global $db;
+
+    $errors = validateAdmin($admin);
+    if (!empty($errors)) {
+        return $errors;
+    }
+    unset($admin['confirmPassword']);
+    $statement = $db->prepare("INSERT INTO admins (first_name, last_name, username, email, password) VALUES (:firstName, :lastName, :username, :email, :password)");
+    $statement->execute($admin);
+    $id = $db->lastInsertId();
+    $result = ['lastInsertId' => $id, 'affectedRows' => $statement];
+    return $result;
 }
