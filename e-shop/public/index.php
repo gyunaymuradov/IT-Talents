ad50d3c6badd965af499009146f404082d7c9ec7
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-25 09:19:30 +0300

    started work on the index page for the public

--- e-shop/public/index.php
+++ e-shop/public/index.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: GYUNAY
- * Date: 9/16/17
- * Time: 7:17 PM
- */
+
+require_once '../private/public_functions.php';
+require_once '../private/shared/public_header.php';
+require_once 'navigation.php';
+
+
+
+
+
+if  (isset($_GET['page'])) {
+    if ($_GET['page'] == 'about_us') {
+        require_once 'about_us.php';
+    } else if ($_GET['page'] == 'contacts') {
+        require_once 'contacts.php';
+    } else if ($_GET['page'] == 'product') {
+        require_once 'product.php';
+    } else if ($_GET['page'] == 'catalog') {
+        require_once 'catalog.php';
+    } else if ($_GET['page'] == 'blog') {
+        require_once 'blog.php';
+    }else if ($_GET['page'] == 'news') {
+        require_once 'news.php';
+    }
+} else {
+    require_once 'home.php';
+}
+
+
+
+
+
+
+require_once '../private/shared/public_footer.php';
