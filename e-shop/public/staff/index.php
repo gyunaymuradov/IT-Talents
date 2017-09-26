63ffc158c4a62e6f118cf8b321313776530f06bd
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-26 11:45:29 +0300

    minor changes

--- e-shop/public/staff/index.php
+++ e-shop/public/staff/index.php
@@ -8,17 +8,11 @@ $pageTitle = "Products";
 require_once '../../private/shared/staff_header.php';
 
 ?>
-
-
     <fieldset>
         <legend>Menu</legend>
             <a href="products">Products</a>
             <a href="news">News</a>
             <a href="admins">Admins</a>
     </fieldset>
-
-
-
-
 
 <?php require_once '../../private/shared/staff_footer.php'; ?>
