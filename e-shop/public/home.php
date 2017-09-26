88c5a04c1096c65836f59d60c4fb10286b9668ca
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-09-26 11:44:53 +0300

    home page now displaying the 4 lastly added products and info about the website

--- e-shop/public/home.php
+++ e-shop/public/home.php
@@ -1,21 +1,36 @@
-<main class="main-content">
-    <div class="content-container">
-        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.</p>
-        <div class="article-container">
+<main>
+    <div class="main-content">
+        <div class="content-container">
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.</p>
+            <br>
+            <h4 id="homepage-title">
+                <p>Check out our freshly added products!</p>
+                <hr>
+            </h4>
+                <?php
 
+                $productsSet = getFreshlyAddedProducts();
+                while($products = $productsSet->fetch(PDO::FETCH_ASSOC)) {
+                    $title = $products['title'];
+                    $price = $products['price'];
+                    $image = substr($products['image'], 6);
+                    $id = $products['id'];
+
+                    echo "<div class=\"article-container left\">
+                                <a href=\"?page=product&id=$id\">
+                                    <div class='sub-container'>
+                                        <article>
+                                            <div>
+                                                <img class=\"homepage-img\" width='200' height='210' src=\"$image\">
+                                            </div>
+                                            <p class=\"price-title\">$title</p><hr>
+                                            <p class=\"price-title\">$$price</p>
+                                            </div> 
+                                        </article>
+                                  </a> 
+                         </div>";
+                }
+                ?>
         </div>
-        <?php
-        echo "<h3 class=\"clear\">Latest News</h3>";
-        $counter = 1;
-        foreach ($newsArr as $news) {
-            if ($counter == 4) {
-                break;
-            }
-            $newsId = $news["id"];
-            $new = substr($news["description"], 0, 281) . "...";
-            echo "<p class=\"clear\">$new<a class=\"link\" href=\"?page=news&id=$newsId\">Read More</a></p>";
-            $counter++;
-        }
-        ?>
     </div>
 </main>
