feb65a777a98fb1297115e138df933055fbab9ee
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-10-08 18:43:29 +0300

    added news dir with index so when clicked on from admin panel won't show error

---
+++ e-shop/public/staff/news/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once '../../../private/initialize.php';
+
+requireLogin();
+
+$pageTitle = "News";
+
+require_once '../../../private/shared/staff_header.php';
+
+?>
+
+
+<div>
+    <br>
+    <h4><a href="../../../public/staff/index.php">Menu</a></h4>
+    <h2>Under reconstruction...</h2>
+</div>
+
+<?php require_once '../../../private/shared/staff_footer.php'; ?>
