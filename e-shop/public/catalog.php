eaf271f4cd2db6bd1657b93cea27b012d7adec09
Gyunay Muradov <GYUNAY@Gyunays-MacBook-Pro.local>
2017-10-08 20:49:57 +0300

    removed leftovers from php paging and fetching

--- e-shop/public/catalog.php
+++ e-shop/public/catalog.php
@@ -1,28 +1,8 @@
-<?php
-
-$productsCountResult = getProductsCount();
-$productsCount = $productsCountResult['count'];
-$pagesCount = ceil($productsCount / 4);
-$page = '';
-
-$page = $_GET['pageNumber'] ?? 1;
-
-if ($page > $pagesCount) {
-    $page = $pagesCount;
-} elseif  ($page <= 0) {
-    $page = 1;
-}
-
-$offset = $page * 4 - 4;
-$products = getAllProducts($offset);
-
-
-?>
 <main>
     <div class="main-content">
         <div class="content-container">
             <div id="search-order">
-                <input type="text" id="search" placeholder="search for product">&nbsp;&nbsp;
+                <input type="text" id="search" onkeyup="searchProducts()" placeholder="search for product">&nbsp;&nbsp;
                 Order by:
                 <button onclick="changeOrder(this.value)" value="price_asc">Price asc</button>&nbsp;&nbsp;
                 <button onclick="changeOrder(this.value)" value="price_desc">Price desc</button>&nbsp;&nbsp;
@@ -31,16 +11,6 @@ $products = getAllProducts($offset);
             </div>
             <br>
             <div id="products-list">
-                <?php
-                foreach ($products as $product) {
-                    $title = htmlEscape($product['title']);
-                    $price = htmlEscape($product['price']);
-                    $image = htmlEscape(substr($product['image'], 6));
-                    $description = htmlEscape(mb_substr($product['description'], 0, 195) . "...");
-                    $id = $product['id'];
-
-                }
-                    ?>
             </div>
             <div id="pagination">
                 <button onclick="previousPage()" class="previous round">&#8249;&#8249; </button>
@@ -48,7 +18,6 @@ $products = getAllProducts($offset);
             </div>
         </div>
         <script src="../private/script.js">
-            loadProducts()
         </script>
     </div>
 </main>
